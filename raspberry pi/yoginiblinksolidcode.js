f8b90ffd5b7046bbe258c59504e4895e53e65f85
Yogini Vazirani <yoginivazirani@gmail.com>
2024-04-22 17:21:32 -0700

    yogini - 2 led update

--- raspberry pi/yoginiblinksolidcode.js
+++ raspberry pi/yoginiblinksolidcode.js
@@ -2,108 +2,112 @@
 const Gpio = require('onoff').Gpio;
 
 // Define GPIO pins for the LEDs and buttons
-const solidLED_D1 = new Gpio(4, 'out');    // First LED connected to GPIO pin 4, set as output
-const solidLED_D2 = new Gpio(7, 'out');
-const blinkLED_D1 = new Gpio(6, 'out');   // Second LED connected to GPIO pin 6, set as output
-const blinkLED_D2 = new Gpio(8, 'out');
-const solid_D1 = new Gpio(3, 'in'); // First button connected to GPIO pin 3, set as input
-const solid_D2 = new Gpio(10, 'in');
-const blinking_D1 = new Gpio(5, 'in'); // Second button connected to GPIO pin 5, set as input
-const blinking_D2 = new Gpio(11, 'in');
-const off_D1 = new Gpio(12, 'in'); // Third button connected to GPIO pin 7, set as input
-const off_D2 = new Gpio(13, 'in');
+const dilutionLED = new Gpio(4, 'out');    // LED for dilution connected to GPIO pin 4, set as output
+const glassLED = new Gpio(7, 'out');       // LED for glass connected to GPIO pin 7, set as output
+const solidButton_dilution = new Gpio(3, 'in');     // Button for solid state of dilution connected to GPIO pin 3, set as input
+const solidButton_glass = new Gpio(10, 'in');        // Button for solid state of glass connected to GPIO pin 10, set as input
+const blinkingButton_dilution = new Gpio(5, 'in');   // Button for blinking state of dilution connected to GPIO pin 5, set as input
+const blinkingButton_glass = new Gpio(11, 'in');      // Button for blinking state of glass connected to GPIO pin 11, set as input
+const offButton_dilution = new Gpio(12, 'in');      // Button for turning off dilution connected to GPIO pin 12, set as input
+const offButton_glass = new Gpio(13, 'in');          // Button for turning off glass connected to GPIO pin 13, set as input
 
 // Function to turn off both LEDs
 function turnOffLEDs() {
-    solidLED_D1.writeSync(0);
-    solidLED_D2.writeSync(0);
-    blinkLED_D1.writeSync(0);
-    blinkLED_D2.writeSync(0);
+    dilutionLED.writeSync(0);
+    glassLED.writeSync(0);
 }
 
-// Event listener for the first solid button to toggle solid state for both LEDs
-solid_D1.watch((err, value) => {
+// Function to make the LED blink
+function blinkLED(led) {
+    let isOn = 0;
+    return setInterval(() => {
+        isOn = isOn ? 0 : 1;
+        led.writeSync(isOn);
+    }, 500); // 500ms interval for blinking
+}
+
+let blinkInterval_dilution = null;
+let blinkInterval_glass = null;
+
+// Event listener for the solid button of dilution to toggle solid state for dilution LED
+solidButton_dilution.watch((err, value) => {
     if (err) {
         throw err;
     }
     if (value === 0) {
-        solidLED_D1.writeSync(1);
-        solidLED_D2.writeSync(1);
-        blinkLED_D1.writeSync(0);
-        blinkLED_D2.writeSync(0);
+        clearInterval(blinkInterval_dilution); // Stop blinking if it's ongoing
+        dilutionLED.writeSync(1);
     }
 });
 
-// Event listener for the second solid button to toggle solid state for both LEDs
-solid_D2.watch((err, value) => {
+// Event listener for the solid button of glass to toggle solid state for glass LED
+solidButton_glass.watch((err, value) => {
     if (err) {
         throw err;
     }
     if (value === 0) {
-        solidLED_D1.writeSync(1);
-        solidLED_D2.writeSync(1);
-        blinkLED_D1.writeSync(0);
-        blinkLED_D2.writeSync(0);
+        clearInterval(blinkInterval_glass); // Stop blinking if it's ongoing
+        glassLED.writeSync(1);
     }
 });
 
-// Event listener for the first blinking button to toggle blinking state for both LEDs
-blinking_D1.watch((err, value) => {
+// Event listener for the blinking button of dilution to toggle blinking state for dilution LED
+blinkingButton_dilution.watch((err, value) => {
     if (err) {
         throw err;
     }
     if (value === 0) {
-        blinkLED_D1.writeSync(1);
-        blinkLED_D2.writeSync(1);
-        solidLED_D1.writeSync(0);
-        solidLED_D2.writeSync(0);
+        turnOffLEDs(); // Turn off LEDs before blinking
+        clearInterval(blinkInterval_dilution); // Stop any previous blinking
+        blinkInterval_dilution = blinkLED(dilutionLED); // Start blinking for dilution LED
+        glassLED.writeSync(1); // Turn on glass LED solidly
     }
 });
 
-// Event listener for the second blinking button to toggle blinking state for both LEDs
-blinking_D2.watch((err, value) => {
+// Event listener for the blinking button of glass to toggle blinking state for glass LED
+blinkingButton_glass.watch((err, value) => {
     if (err) {
         throw err;
     }
     if (value === 0) {
-        blinkLED_D1.writeSync(1);
-        blinkLED_D2.writeSync(1);
-        solidLED_D1.writeSync(0);
-        solidLED_D2.writeSync(0);
+        turnOffLEDs(); // Turn off LEDs before blinking
+        clearInterval(blinkInterval_glass); // Stop any previous blinking
+        blinkInterval_glass = blinkLED(glassLED); // Start blinking for glass LED
+        dilutionLED.writeSync(1); // Turn on dilution LED solidly
     }
 });
 
-// Event listener for the first off button to turn off both LEDs
-off_D1.watch((err, value) => {
+// Event listener for the off button of dilution to turn off dilution LED
+offButton_dilution.watch((err, value) => {
     if (err) {
         throw err;
     }
     if (value === 0) {
-        turnOffLEDs();
+        clearInterval(blinkInterval_dilution); // Stop blinking if ongoing
+        dilutionLED.writeSync(0); // Turn off dilution LED
     }
 });
 
-// Event listener for the second off button to turn off both LEDs
-off_D2.watch((err, value) => {
+// Event listener for the off button of glass to turn off glass LED
+offButton_glass.watch((err, value) => {
     if (err) {
         throw err;
     }
     if (value === 0) {
-        turnOffLEDs();
+        clearInterval(blinkInterval_glass); // Stop blinking if ongoing
+        glassLED.writeSync(0); // Turn off glass LED
     }
 });
 
 // Clean up GPIO resources on program exit
 process.on('SIGINT', () => {
     turnOffLEDs();
-    solidLED_D1.unexport();
-    solidLED_D2.unexport();
-    blinkLED_D1.unexport();
-    blinkLED_D2.unexport();
-    solid_D1.unexport();
-    solid_D2.unexport();
-    blinking_D1.unexport();
-    blinking_D2.unexport();
-    off_D1.unexport();
-    off_D2.unexport();
+    dilutionLED.unexport();
+    glassLED.unexport();
+    solidButton_dilution.unexport();
+    solidButton_glass.unexport();
+    blinkingButton_dilution.unexport();
+    blinkingButton_glass.unexport();
+    offButton_dilution.unexport();
+    offButton_glass.unexport();
 });
